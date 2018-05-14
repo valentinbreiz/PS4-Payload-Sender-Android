0dde58166f085995543c106472db48fb79d40502
Valentin Charbonnier <valentinbreiz@gmail.com>
2018-05-14 21:55:46 +0200

    Now working!

--- app/src/main/java/valentinbreiz/ps4payloadsender/Connection.java
+++ app/src/main/java/valentinbreiz/ps4payloadsender/Connection.java
@@ -1,6 +1,20 @@
 package valentinbreiz.ps4payloadsender;
 
+import android.Manifest;
+import android.content.ContentUris;
+import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.net.Uri;
+import android.os.AsyncTask;
+import android.os.Build;
+import android.os.Environment;
+import android.provider.DocumentsContract;
+import android.provider.MediaStore;
+import android.support.annotation.RequiresApi;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.content.DialogInterface;
@@ -11,11 +25,14 @@ import android.app.AlertDialog;
 import android.widget.TextView;
 
 import java.io.BufferedInputStream;
+import java.io.ByteArrayInputStream;
 import java.io.DataOutputStream;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.io.OutputStream;
+import java.io.PrintWriter;
 import java.net.InetAddress;
 import java.net.InetSocketAddress;
 import java.net.Socket;
@@ -32,6 +49,18 @@ public class Connection extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_connection);
+        if (ContextCompat.checkSelfPermission(this,
+                Manifest.permission.READ_EXTERNAL_STORAGE)
+                != PackageManager.PERMISSION_GRANTED) {
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+                    Manifest.permission.READ_EXTERNAL_STORAGE)) {
+            } else {
+                ActivityCompat.requestPermissions(this,
+                        new String[]{Manifest.permission.READ_EXTERNAL_STORAGE},
+                        MY_PERMISSIONS_REQUEST_READ_FILES);
+            }
+        } else {
+        }
 
         ConnectBtn = (Button) findViewById(R.id.ConnectBtn);
         SendBtn = (Button) findViewById(R.id.SendPayloadBtn);
@@ -45,16 +74,38 @@ public class Connection extends AppCompatActivity {
 
     }
 
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case MY_PERMISSIONS_REQUEST_READ_FILES: {
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                } else {
+                    System.exit(0);
+                }
+                return;
+            }
+        }
+    }
+
+    final int MY_PERMISSIONS_REQUEST_READ_FILES = 0;
+
+    private Send send;
+
     private Socket socket;
     private byte[] payload;
-    DataOutputStream dataOutputStream = null;
+    private ConnectionConfig params;
+
+    private String ip;
+    private int port;
 
     protected void Connect(View view)
     {
-        final EditText ip = (EditText) findViewById(R.id.IPAddressTextBox);
-        final EditText port = (EditText) findViewById(R.id.PortTextBox);
+        final EditText ips = (EditText) findViewById(R.id.IPAddressTextBox);
+        final EditText ports = (EditText) findViewById(R.id.PortTextBox);
 
-        if (ip.getText().toString().equals("") || port.getText().toString().equals("")) {
+        if (ips.getText().toString().equals("") || ports.getText().toString().equals("")) {
             new AlertDialog.Builder(this)
                     .setTitle("Error!")
                     .setMessage("You need to type an IP Address or a Port.")
@@ -66,25 +117,11 @@ public class Connection extends AppCompatActivity {
         }
         else
         {
-            try
-            {
-                socket = new Socket();
-                socket.setTcpNoDelay(true);
-                socket.connect(new InetSocketAddress(ip.getText().toString(), Integer.parseInt(port.getText().toString())));
+            ip = ips.getText().toString();
+            port = Integer.parseInt(ports.getText().toString());
 
-                dataOutputStream = new DataOutputStream(socket.getOutputStream());
-            }
-            catch (UnknownHostException e1)
-            {
-                e1.printStackTrace();
-            }
-            catch (IOException e1)
-            {
-                e1.printStackTrace();
-            }
-
-            ip.setEnabled(false);
-            port.setEnabled(false);
+            ips.setEnabled(false);
+            ports.setEnabled(false);
             ConnectBtn.setEnabled(false);
             BrowseBtn.setEnabled(true);
         }
@@ -103,13 +140,14 @@ public class Connection extends AppCompatActivity {
         startActivityForResult(intent, PICKFILE_RESULT_CODE);
     }
 
+    @RequiresApi(api = Build.VERSION_CODES.KITKAT)
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         switch(requestCode){
             case PICKFILE_RESULT_CODE:
                 if(resultCode==RESULT_OK){
-                    String FilePath = data.getData().getPath();
-
+                    Uri uri = data.getData();
+                    String FilePath = Utils.getActualPath(this, uri);
                     BrowseBtn.setText(FilePath);
                     File file = new File(FilePath);
                     int size = (int) file.length();
@@ -135,24 +173,197 @@ public class Connection extends AppCompatActivity {
     public void Send(View view)
     {
         Injecting.setVisibility(TextView.VISIBLE);
-
-        try
+        if (payload != null)
         {
-            if (payload == null)
-            {
-                Injecting.setText("Error, file is null!");
-            }
-            else
-            {
-                dataOutputStream.write(payload);
-                Injecting.setText("Done!");
-            }
-            socket.close();
+            params = new ConnectionConfig(ip, port, payload);
+            send = new Send();
+            send.execute(params);
         }
-        catch (IOException e)
+        else
         {
-            Injecting.setText("Error!");
-            e.printStackTrace();
+            Injecting.setText("Error, file is null >< !!");
+        }
+    }
+
+    private static class ConnectionConfig {
+        String ip;
+        int port;
+        byte[] data;
+
+        ConnectionConfig(String ip, int port, byte[] data) {
+            this.ip = ip;
+            this.port = port;
+            this.data = data;
+        }
+    }
+
+    private DataOutputStream dOut;
+    private class Send extends AsyncTask<ConnectionConfig, byte[], Void>
+    {
+        @Override
+        protected Void doInBackground(ConnectionConfig... params)
+        {
+            try
+            {
+                String ip = params[0].ip;
+                int port = params[0].port;
+                byte[] data = params[0].data;
+                socket = new Socket(ip, port);
+                dOut = new DataOutputStream(socket.getOutputStream());
+                dOut.write(data);
+                socket.close();
+            }
+            catch (UnknownHostException e1)
+            {
+                Intent i = getBaseContext().getPackageManager()
+                        .getLaunchIntentForPackage( getBaseContext().getPackageName() );
+                i.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                startActivity(i);
+                e1.printStackTrace();
+            }
+            catch (IOException e1)
+            {
+                Intent i = getBaseContext().getPackageManager()
+                        .getLaunchIntentForPackage( getBaseContext().getPackageName() );
+                i.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                startActivity(i);
+                e1.printStackTrace();
+            }
+
+            return null;
+        }
+
+        @Override
+        protected void onProgressUpdate(byte[]... values) {
+            Injecting.setText("Injecting...");
+        }
+
+        @Override
+        protected void onPostExecute(Void voidd) {
+            Injecting.setText("Done!");
+        }
+    }
+
+    public static class Utils{
+        @RequiresApi(api = Build.VERSION_CODES.KITKAT)
+        public static String getActualPath(final Context context, final Uri uri) {
+
+            final boolean isKitKat = Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT;
+
+            // DocumentProvider
+            if (isKitKat && DocumentsContract.isDocumentUri(context, uri)) {
+                // ExternalStorageProvider
+                if (isExternalStorageDocument(uri)) {
+                    final String docId = DocumentsContract.getDocumentId(uri);
+                    final String[] split = docId.split(":");
+                    final String type = split[0];
+
+                    if ("primary".equalsIgnoreCase(type)) {
+                        return Environment.getExternalStorageDirectory() + "/" + split[1];
+                    }
+
+                    // TODO handle non-primary volumes
+                }
+                // DownloadsProvider
+                else if (isDownloadsDocument(uri)) {
+
+                    final String id = DocumentsContract.getDocumentId(uri);
+                    final Uri contentUri = ContentUris.withAppendedId(
+                            Uri.parse("content://downloads/public_downloads"), Long.valueOf(id));
+
+                    return getDataColumn(context, contentUri, null, null);
+                }
+                // MediaProvider
+                else if (isMediaDocument(uri)) {
+                    final String docId = DocumentsContract.getDocumentId(uri);
+                    final String[] split = docId.split(":");
+                    final String type = split[0];
+
+                    Uri contentUri = null;
+                    if ("image".equals(type)) {
+                        contentUri = MediaStore.Images.Media.EXTERNAL_CONTENT_URI;
+                    } else if ("video".equals(type)) {
+                        contentUri = MediaStore.Video.Media.EXTERNAL_CONTENT_URI;
+                    } else if ("audio".equals(type)) {
+                        contentUri = MediaStore.Audio.Media.EXTERNAL_CONTENT_URI;
+                    }
+
+                    final String selection = "_id=?";
+                    final String[] selectionArgs = new String[] {
+                            split[1]
+                    };
+
+                    return getDataColumn(context, contentUri, selection, selectionArgs);
+                }
+            }
+            // MediaStore (and general)
+            else if ("content".equalsIgnoreCase(uri.getScheme())) {
+                return getDataColumn(context, uri, null, null);
+            }
+            // File
+            else if ("file".equalsIgnoreCase(uri.getScheme())) {
+                return uri.getPath();
+            }
+
+            return null;
+        }
+
+        /**
+         * Get the value of the data column for this Uri. This is useful for
+         * MediaStore Uris, and other file-based ContentProviders.
+         *
+         * @param context The context.
+         * @param uri The Uri to query.
+         * @param selection (Optional) Filter used in the query.
+         * @param selectionArgs (Optional) Selection arguments used in the query.
+         * @return The value of the _data column, which is typically a file path.
+         */
+        private static String getDataColumn(Context context, Uri uri, String selection,
+                                            String[] selectionArgs) {
+
+            Cursor cursor = null;
+            final String column = "_data";
+            final String[] projection = {
+                    column
+            };
+
+            try {
+                cursor = context.getContentResolver().query(uri, projection, selection, selectionArgs,
+                        null);
+                if (cursor != null && cursor.moveToFirst()) {
+                    final int column_index = cursor.getColumnIndexOrThrow(column);
+                    return cursor.getString(column_index);
+                }
+            } finally {
+                if (cursor != null)
+                    cursor.close();
+            }
+            return null;
+        }
+
+
+        /**
+         * @param uri The Uri to check.
+         * @return Whether the Uri authority is ExternalStorageProvider.
+         */
+        public static boolean isExternalStorageDocument(Uri uri) {
+            return "com.android.externalstorage.documents".equals(uri.getAuthority());
+        }
+
+        /**
+         * @param uri The Uri to check.
+         * @return Whether the Uri authority is DownloadsProvider.
+         */
+        public static boolean isDownloadsDocument(Uri uri) {
+            return "com.android.providers.downloads.documents".equals(uri.getAuthority());
+        }
+
+        /**
+         * @param uri The Uri to check.
+         * @return Whether the Uri authority is MediaProvider.
+         */
+        public static boolean isMediaDocument(Uri uri) {
+            return "com.android.providers.media.documents".equals(uri.getAuthority());
         }
     }
 }
